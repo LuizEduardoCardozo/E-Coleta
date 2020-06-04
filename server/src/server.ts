b0da6e726865fa7c640ef937396a504fac5a63cb
LuizEduardoCardozo <eduard.cardoz@gmail.com>
2020-06-04 14:49:35 -0300

    second day

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,4 +2,17 @@ import express from 'express';
 
 const app = express();
 
+/*
+    Arquivos do tipo .ts não podem ser executados pelo node. Para isso, é necessário um pacote extra, chamado
+    "ts-node"
+
+    É preciso ter um arquivo de configurações do typescript toda vez que for usar-lo
+    (npx tsc --init) para criar esse arquvo
+
+    Após ter o typescript e o ts-node instalados, para rodar o servidor, basta usar (npx ts-node <arquivo>)
+
+*/
+
+app.get('/users', (req,res) => console.log("Olá, mundo!"));
+
 app.listen(3000);
